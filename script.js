97c6a113b61fdafdd2fb46c6a55a7971615defde
FreSum123 <sumfree123@gmail.com>
2026-02-14 19:11:55 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log("Website loaded successfully!");
+let records = JSON.parse(localStorage.getItem("records")) || [];
+
+function render() {
+  const table = document.getElementById("records");
+  table.innerHTML = "";
+
+  records.forEach(r => {
+    table.innerHTML += `
+      <tr>
+        <td>${r.item}</td>
+        <td>${r.type}</td>
+        <td>${r.desc}</td>
+        <td>$${r.amount}</td>
+      </tr>
+    `;
+  });
+}
+
+function addRecord() {
+  const record = {
+    item: document.getElementById("item").value,
+    type: document.getElementById("type").value,
+    desc: document.getElementById("desc").value,
+    amount: document.getElementById("amount").value
+  };
+
+  records.push(record);
+  localStorage.setItem("records", JSON.stringify(records));
+  render();
+}
+
+render();
